8a26944e16d6307cad4be408e8430240a33ac2a5
xiao <im.qxxiao@gmail.com>
2021-10-29 11:54:42 +0800

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={"http://pic.qxxiao.cn/20211029114229.png"} className="App-logo" alt="logo" />
+        <p>
+          IPFS-File-Load
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
